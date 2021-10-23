e7a3ec6b131f0fff722ca3a70504704125a34288
federicoestevez <federico.estevez21@gmail.com>
2021-10-23 17:38:57 -0300

    Socket.io server
    Co-Authored-By: LuisOsGon <76964406+LuisOsGon@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import path from "path";
+import http from "http";
 import express from "express";
+import { Server } from "socket.io";
 
 import api from "./server/routes";
 import connectDB from "./server/utils/db";
 
-
-const app = express();
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 const startApp = async () => {
+  const app = express();
+  const server = http.createServer(app);
+
+  const io = new Server(server);
+
   await connectDB(process.env.DB_URL);
 
   app.use(express.json());
@@ -16,8 +21,12 @@ const startApp = async () => {
   app.use("/api", api);
   app.use("*", express.static(path.join(__dirname, 'web/public')));
 
-  app.listen(port, () => {
-    console.log(`Servidor ejecutandose en http://localhost:${port}`);
+  io.on("connection", socket => {
+    console.log("Usuario conectado");
+  })
+
+  server.listen(PORT, () => {
+    console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
   });
 }
 
