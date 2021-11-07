fee1c83d098e3c99c45b41f4f37561bea7420162
federicoestevez <federico.estevez21@gmail.com>
2021-11-07 11:47:50 -0300

    wip

--- web/src/pages/Room.js
+++ web/src/pages/Room.js
@@ -1,4 +1,5 @@
-import React from "react";
+import io from "socket.io-client";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RoomInfo from "../components/RoomInfo";
 import Members from "../components/Members";
@@ -7,14 +8,23 @@ import Messages from "../components/Messages";
 
 function Room() {
   const { roomId } = useParams();
+  const [initialized, setInitialized] = useState(false);
 
-  console.log(
-    "**************************************************************************\n\n\n"
-  );
-  console.log(roomId);
-  console.log(
-    "\n\n\n**************************************************************************"
-  );
+  const connectToRoom = () => {
+    const socket = io(`http://localhost:3001/room/${roomId}`);
+    socket.on("connect", () => {
+      setInitialized(true);
+    });
+    socket.on("message", (message) => {
+      console.log(message);
+    });
+  };
+
+  useEffect(() => {
+    if (!initialized) {
+      connectToRoom();
+    }
+  }, [initialized]);
 
   return (
     <div className="Room">
